c5dcc3f4b71aa0099c3b1cd4e5e7bacfc9d9656c
DatDangg <datdang20112003@gmail.com>
2025-06-10 22:58:52 +0700

    .

--- pages/api/blogs.js
+++ pages/api/blogs.js
@@ -2,6 +2,13 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   const filePath = path.join(process.cwd(), 'data', 'db.json');
   const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'));
   res.status(200).json(data.blog);
--- pages/api/db.js
+++ pages/api/db.js
@@ -2,6 +2,13 @@ import fs from 'fs';
 import path from 'path';
 
 export default function handler(req, res) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   try {
     const filePath = path.join(process.cwd(), 'data', 'db.json');
     const fileContents = fs.readFileSync(filePath, 'utf-8');
